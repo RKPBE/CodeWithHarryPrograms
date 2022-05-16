6ad3f5c4d1eb5e857976d3202f197cf63650e18d
RKPBE <101239646+RKPBE@users.noreply.github.com>
2022-05-16 12:36:50 +0530

    Add files via upload

---
+++ AdditingTemplateToTheFile/Information.py
@@ -0,0 +1,12 @@
+# I have Created this File this is about the set up of Django
+#1.Creat new Project
+#2.run the command on cmd or terminal window of pycharm django-admin---->to see it is properly working
+#3.go to folder where manage.py file located
+#4.Change the dir directory in setting.py Folder add "template" in it.
+#5.import render()library in views file
+#6.create template directory where your manage.py locate
+#7.just like your working folder name then ur template folder then db.sqlite then manage.py
+#8.If it gives error then move your template folder up click on restore and again run
+#9.In template directory add "index.html"  html file
+#10.Add line "Template is Working" in index.html file
+#11.Add text in body like "I am Working Kajal".
---
+++ AdditingTemplateToTheFile/views.py
@@ -0,0 +1,23 @@
+#I have created this file for displaying name on wabe page it is a Second Program
+
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return HttpResponse("About KAJAL<a href='/'> back </a>")
+
+def removepunc(request):
+    return HttpResponse("removepunc KAJAL <a href='/'> back </a>")
+
+def capitalizefirst(request):
+    return HttpResponse("capitalizefirst KAJAL <a href='/'> back </a>")
+
+def newlineremove(request):
+    return HttpResponse("newlineremove KAJAL <a href='/'> back </a>")
+
+def spaceremove(request):
+    return HttpResponse("spaceremove KAJAL <a href='/'> back </a>")
