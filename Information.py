c7bfc10b2eeda31b2d3d69af040ea2d7e547508d
RKPBE <101239646+RKPBE@users.noreply.github.com>
2022-05-16 10:19:24 +0530

    Add files via upload

---
+++ Information.py
@@ -0,0 +1,5 @@
+# I have Created this File this is about the set up of Django
+#1.Creat new Project
+#2.run the command on cmd or terminal window of pycharm django-admin---->to see it is properly working
+#3.go to folder where manage.py file located
+#4.run the command "python manage.py runserver"
